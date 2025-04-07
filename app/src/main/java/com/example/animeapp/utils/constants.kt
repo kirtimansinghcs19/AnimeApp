02289b41adc5cdb11c59f533c225c9005f5a8115
kirtimancs19 <kirtiman.singh@kapturecrm.com>
2025-04-07 16:17:32 +0530

    Revert "first commit"
    This reverts commit 79ce4aab4102d507a529460e518ff59eeb507a91.

--- app/src/main/java/com/example/animeapp/utils/constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.animeapp.utils
-
-
-object Constants {
-    const val ANIME_ID = "anime_id"
-    const val YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v="
-    // Add more constants as needed
-}
